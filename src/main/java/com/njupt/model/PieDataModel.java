667fbf265843e4d4bef9946c8081aa768e14b2a6
weixiaokang <weixkz@gmail.com>
2017-05-18 00:52:04 +0800

    添加折线图、柱状图、饼图页面，修改页面显示bug

---
+++ src/main/java/com/njupt/model/PieDataModel.java
@@ -0,0 +1,29 @@
+package com.njupt.model;
+
+/**
+ * @author weixk
+ * @version Created time 17/5/17. Last-modified time 17/5/17.
+ */
+public class PieDataModel {
+    private String name;
+    private Integer value;
+
+    public PieDataModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
